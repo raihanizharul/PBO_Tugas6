20f592475c033b16dd8059a0d8c63bcc23817c87
Muhammad Raihan Izharul Haq <94726321+raihanizharul@users.noreply.github.com>
2022-11-07 16:39:01 +0700

    Add files via upload

---
+++ LaptopKu/Laptop.java
@@ -0,0 +1,11 @@
+package Nusput.P6.Laptopku;
+
+public interface Laptop {
+    int MAX_VOL=100;
+    int MIN_VOL=0;
+
+    void powerOn();
+    void powerOff();
+    void volumeUp();
+    void volumeDown();
+}
